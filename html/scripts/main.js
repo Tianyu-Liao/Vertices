5fcaaf8e1f0e6bd7e0c9e717e070dc08e3daf337
Tianyu-Liao <m.sc.tianyu.liao@gmail.com>
2020-02-20 08:30:20 +0100

    Init Django

--- html/scripts/main.js
+++ html/scripts/main.js
@@ -34,3 +34,21 @@ myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
 myButton.onclick = function() {
     setUserName();
  }
+
+ 
+3
+4
+5
+6
+7
+8
+9
+var mapType = google.maps.MapTypeId.ROADMAP;
+var lat = 39.915168, lng = 116.403875, zoom = 10;
+var mapOptions = {
+    center: new google.maps.LatLng(lat, lng),  //地图的中心点
+    zoom: zoom,               　　　　　　　　　　//地图缩放比例
+    mapTypeId: mapType,       　　　　　　　　　　//指定地图展示类型：卫星图像、普通道路
+    scrollwheel: true          　　　　　　　　　 //是否允许滚轮滑动进行缩放
+};
+var map = new google.maps.Map(document.getElementById("map"), mapOptions); //创建谷歌地图
